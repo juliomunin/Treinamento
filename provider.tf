55d3e59016473adeffb538a37649d6388b705db4
JulioMunin <45806461+juliomunin@users.noreply.github.com>
2022-11-08 22:21:33 -0300

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = var.region
-    access_key  = var.access_key
-    secret_key  = var.secret_key
+    region = region
+    access_key  = access_key
+    secret_key  = secret_key
 }
